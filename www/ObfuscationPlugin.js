c610161a94b04e18e7bfa940bf7ad06fe8600276
Paulo Camilo <pcamilo@innovagency.com>
2019-11-04 11:03:01 +0000

    Created a object to Plugin

--- www/ObfuscationPlugin.js
+++ www/ObfuscationPlugin.js
@@ -1,1 +1,15 @@
+var cordova = require('cordova');
 var exec = require('cordova/exec');
+
+/**
+* Constructor.
+*
+* @returns {ObfuscationPlugin}
+*/
+function ObfuscationPlugin() {
+
+};
+
+//create instance of ObfuscationPlugin
+var ObfuscationPlugin = new ObfuscationPlugin();
+module.exports = ObfuscationPlugin;
